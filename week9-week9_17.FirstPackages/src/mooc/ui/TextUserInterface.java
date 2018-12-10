ef285005494686dfca390c287f95a92d61b52158
demciucvictor <40291128+demciucvictor@users.noreply.github.com>
2018-12-10 21:05:56 +0200

    Add files via upload

---
+++ week9-week9_17.FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,12 @@
+
+package mooc.ui;
+
+
+
+public class TextUserInterface implements UserInterface{
+    
+    public void update(){   
+        System.out.println("Updating the user interface");
+    }
+    
+}
